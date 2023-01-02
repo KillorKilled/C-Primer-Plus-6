e489e0b489ec5074544d3fae3a6328d9f55fd445
QiRui <1493329656@qq.com>
2023-01-02 23:47:09 +0800

    第一章
    dev-c++写的中文识别不了

---
+++ ch01/1.7.3.cpp
@@ -0,0 +1,13 @@
+//1.7.3
+//通过这段代码学习的范例
+#include <stdio.h>
+
+int main(void) {
+	int dogs;
+
+	printf("How many dogs do you have?\n ");
+	scanf("%d", &dogs);
+	printf("So you have %d dog(s)!\n", dogs);
+
+	return 0;
+}
